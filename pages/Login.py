a9777996ab6488b6bb91de65ad19435877cda823
Madu Fioranti <00001092043950sp@al.educacao.sp.gov.br>
2026-07-15 11:50:16 -0300

    Update Login.py

--- pages/Login.py
+++ pages/Login.py
@@ -16,7 +16,7 @@ senha = st.text_input("Senha", type="password")
 
 if st.button("Entrar"):
     # Validação simples (você pode colocar as senhas dos alunos aqui ou conectar a um banco)
-    if usuario == "g4@petsaude.br" and senha == "pet123":
+    if usuario == "petsaude@unesp.br" or "petsaude@usp.br" and senha == "pet123":
         st.session_state["logado"] = True
         st.session_state["usuario_nome"] = "Membro G4"
         st.success("Login realizado com sucesso! Vá para a 'Área do Aluno' no menu lateral.")
